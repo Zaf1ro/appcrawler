960d5d7141eec589516c806aad1568a8498d197f
Jason <duan_jason@outlook.com>
2018-05-23 10:31:17 -0400

    change of apple appstore

--- appcrawler/spiders/apple_appstore.py
+++ appcrawler/spiders/apple_appstore.py
@@ -2,8 +2,7 @@
 # coding: utf-8
 
 import scrapy
-from ..items import AppleAppStoreItem
-import datetime
+from ..items import AppleItem
 
 
 # TODO: limit of pages of each category
@@ -41,7 +40,7 @@ XPATH_PAGE = "//*[@id='selectedgenre']/ul[2]//a"
 XPATH_APP_NAME = "//header/h1/text()"   # 6 results
 XPATH_APP_INFO = "//dl[@class='information-list information-list--app medium-columns']//dd"  # 1 results
 XPATH_APP_VERSION = "//p[@class='l-column small-6 medium-12 whats-new__latest__version']"
-XPATH_APP_RATING = "//div/div[1]/h3/span"
+XPATH_APP_RATING = "//figure[@class='we-star-rating ember-view']/@aria-label"
 
 
 class AppStoreSpider(scrapy.Spider):
@@ -54,26 +53,28 @@ class AppStoreSpider(scrapy.Spider):
 
     # start to parse requests
     def start_requests(self):
-        for category_url in CATEGORY.values():
-            yield scrapy.Request(category_url, callback=self.parse)
+        for name, category_url in CATEGORY.items():
+            yield scrapy.FormRequest(
+                category_url,
+                formdata={
+                    'category_name': name
+                },
+                callback=self.parse
+            )
 
     # iterate apps by category
     def parse(self, response):
-
+        yield scrapy.FormRequest(
+            response.url,
+            formdata={
+                'start': '%s' % self.start,
+                'num': '60',
+                'ipf': '1',
+                'xhr': '1',
+            },
+            callback=self.parse_category_page
+        )
         category = response.select('//title/text()').extract()[0].split('-')[0].strip()
-        idx = 0
-        for url, name in zip(hxs.select('//div[contains(@class,"column")]/ul/li/a/@href').extract(), hxs.select('//div[contains(@class,"column")]/ul/li/a/text()').extract()):
-            if not '/app/' in url:
-                continue
-            i = AppItem()
-            i['name'] = name
-            i['url'] = url
-            i['id'] = url.split('/')[-1].split('?')[0]
-            i['category'] = category
-            i['last_update'] = datetime.date.today().isoformat()
-            i['store'] = 'appstore'
-            idx += 1
-            yield i
 
     # iterate apps by initial letter
     def iterate_by_initial_letter(self):
@@ -85,22 +86,21 @@ class AppStoreSpider(scrapy.Spider):
 
     @staticmethod
     def parse_app(response):  # parse_app
-        app_item = AppleAppStoreItem()
+        app_item = AppleItem()
         app_item['name'] = response.xpath(XPATH_APP_NAME).extract()[3]
         app_info = response.xpath(XPATH_APP_INFO).extract()
         app_item['seller'] = app_info[0]
         app_item['size'] = app_info[1]      # erase 'MB'
         app_item['category'] = app_info[2]
-
         # Check for compatibility
         compatibility = app_info[3]
         app_item['is_iphone'] = 'iPhone' in compatibility
         app_item['is_ipad'] = 'iPad' in compatibility
         app_item['is_ipod'] = 'iPod' in compatibility
-
         app_item['languages'] = app_info[4]     # English, Dutch, French, German, Italian
         app_item['age_rating'] = app_info[5]    # 4+
         app_item['price'] = app_info[7]         # Free
+        app_item['rating'] = response.xpath(XPATH_APP_RATING).extract()[0]
 
         yield app_item
 
