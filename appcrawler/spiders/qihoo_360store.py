77ba116f28e69d55be685c52339245f9e038d15a
Jason <duan_jason@outlook.com>
2018-06-02 19:59:40 -0400

    fix bug in 360store

--- appcrawler/spiders/qihoo_360store.py
+++ appcrawler/spiders/qihoo_360store.py
@@ -8,7 +8,7 @@ from scrapy.exceptions import CloseSpider
 from ..items import QihooItem
 
 # Limit of pages
-MAX_PAGES = 10
+MAX_APPS_PER_PAGE = 2
 
 # Category
 CATEGORY = {
@@ -24,7 +24,6 @@ CATEGORY = {
     'education': 'http://zhushou.360.cn/list/index/cid/102232',
     'finance': 'http://zhushou.360.cn/list/index/cid/102139',
     'health': 'http://zhushou.360.cn/list/index/cid/102233',
-
 }
 
 # URL
@@ -39,7 +38,7 @@ XPATH_DOWNLOAD_URL = "//*[@id='app-info-panel']/div/dl/dd/a/@href"  # OK
 XPATH_DOWNLOAD_TIMES = "//*[@id='app-info-panel']/div/dl/dd/div/span[3]/text()"  # OK
 XPATH_APP_NAME = "//*[@id='app-name']/span/@title"  # OK
 XPATH_APP_RATING = "//div[@class='pf']/span[1]/text()"
-XPATH_APP_SIZE = "//div[@class='pf']/span[4]"
+XPATH_APP_SIZE = "//div[@class='pf']/span[4]/text()"
 
 
 class QihooSpider(scrapy.Spider):
@@ -57,12 +56,14 @@ class QihooSpider(scrapy.Spider):
 
     # start with category urls
     def start_requests(self):
-        for name, category_url in CATEGORY:
-            yield Request(
-                url=category_url,
-                callback=self.parse,
-                meta={'category_name': name}
-            )
+        name = 'security'
+        category_url = 'http://zhushou.360.cn/list/index/cid/11'
+        # for name, category_url in CATEGORY.items():
+        yield Request(
+            url=category_url,
+            callback=self.parse,
+            meta={'category_name': name}
+        )
 
     # category -> page number
     def parse(self, response):
@@ -70,12 +71,17 @@ class QihooSpider(scrapy.Spider):
             CloseSpider('Error with network')
 
         category_name = response.meta['category_name']
-        for i in range(1, MAX_PAGES):
+        for i in range(1, MAX_APPS_PER_PAGE):
+
+            file = open('testfile.txt', 'w')
+            file.write(category_name)
+            file.close()
+
             yield Request(
                 url=response.url + '?page=%d' % i,
                 callback=self.parse_page,
                 meta={
-                    'category': category_name,
+                    'category_name': category_name,
                     'page': i
                 }
             )
@@ -83,7 +89,7 @@ class QihooSpider(scrapy.Spider):
     # page number -> app page
     def parse_page(self, response):
         app_urls = response.xpath(XPATH_APP_URL).extract()
-        for url in app_urls:
+        for url in app_urls[0:1]:
             yield Request(
                 url=DOMAIN + url,
                 callback=self.parse_app,
@@ -93,14 +99,15 @@ class QihooSpider(scrapy.Spider):
                 }
             )
 
-    @staticmethod
-    def parse_app(response):
+    def parse_app(self, response):
         app_item = QihooItem()
-        app_item['category'] = response.meta['']
+        app_item['category'] = response.meta['category_name']
         app_item['name'] = response.xpath(XPATH_APP_NAME).extract()[0]
         app_item['rating'] = response.xpath(XPATH_APP_RATING).extract()[0]  # 7.6分
         app_item['apk_url'] = response.xpath(XPATH_DOWNLOAD_URL).extract()[0]
-        app_item['download_count'] = response.xpath(XPATH_DOWNLOAD_TIMES)
-        app_item['apk_size'] = response.xpath(XPATH_APP_SIZE)
+        raw_download_times = response.xpath(XPATH_DOWNLOAD_TIMES).extract()[0]
+        app_item['download_count'] = raw_download_times.spilt(': ')[1]
+        app_item['apk_size'] = response.xpath(XPATH_APP_SIZE).extract()[0]
+
         yield app_item
 
